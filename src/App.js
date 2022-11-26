c68179fa05194f296eb08c1e94ce6e57a4c90fc5
Tomás Grüner <tomas.gruner@lambdaclass.com>
2022-11-26 18:28:00 -0300

    Agrego esqueleto del proyecto
    - agrego dependencias:
      - react-router-dom
      - chakra-ui
      - prettier
    - agrego un routing tentativo en App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Text } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/login" element={<Text>Login</Text>} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Text>Home</Text>} />
+        <Route path="proyectos" element={<Text>Proyectos</Text>} />
+        <Route path="tickets" element={<Text>Tickets</Text>} />
+        <Route path="partesDeHoras" element={<Text>PartesDeHoras</Text>} />
+      </Route>
+    </Routes>
   );
 }
 
