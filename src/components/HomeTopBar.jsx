2460602e556ac1c8c161bda46f86e15b916f3745
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2022-11-26 21:40:21 -0300

    Corro prettier

--- src/components/HomeTopBar.jsx
+++ src/components/HomeTopBar.jsx
@@ -1,10 +1,11 @@
 import { Image } from "@chakra-ui/react";
-import "../styles/home.css"
+import "../styles/home.css";
 
-export default function HomeTopBar(props){
-
-    return <div className="TopDiv">
-        <Image src={require("../styles/PSAIcon.jpg")}/>
-        <p className="UserInfo">Bienvenido Juan Zeo</p>
-    </div>;
+export default function HomeTopBar(props) {
+  return (
+    <div className="TopDiv">
+      <Image src={require("../styles/PSAIcon.jpg")} />
+      <p className="UserInfo">Bienvenido Juan Zeo</p>
+    </div>
+  );
 }
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,35 +1,36 @@
 import React from "react";
 
-import "../styles/home.css"
+import "../styles/home.css";
 import HomeCard from "../components/HomeCard";
 import HomeTopBar from "../components/HomeTopBar";
 import Routing from "../routes/config";
 import useNavigateWParams from "../routes/navigation";
 
-export default function Home(props){
+export default function Home(props) {
+  const navigate = useNavigateWParams();
 
-    const navigate = useNavigateWParams();
+  const toRecursos = () => {
+    navigate(Routing.Recursos);
+  };
 
-    const toRecursos = () => {
-        navigate(Routing.Recursos);
-    };
+  const toSoporte = () => {
+    navigate(Routing.Soporte);
+  };
 
-    const toSoporte = () => {
-        navigate(Routing.Soporte);
-    }
+  const toProyectos = () => {
+    navigate(Routing.Proyectos);
+  };
 
-    const toProyectos = () => {
-        navigate(Routing.Proyectos);
-    }
-
-    return <React.Fragment>
-            <HomeTopBar />
-            <div className="HomeDiv">
-                <HomeCard text="Soporte" onClick={toSoporte}/>
-                <HomeCard text="Proyectos" onClick={toProyectos}/>
-            </div>
-            <div className="HomeDiv">
-                <HomeCard text="Recursos" onClick={toRecursos}/>
-            </div>
-            </React.Fragment>;
+  return (
+    <React.Fragment>
+      <HomeTopBar />
+      <div className="HomeDiv">
+        <HomeCard text="Soporte" onClick={toSoporte} />
+        <HomeCard text="Proyectos" onClick={toProyectos} />
+      </div>
+      <div className="HomeDiv">
+        <HomeCard text="Recursos" onClick={toRecursos} />
+      </div>
+    </React.Fragment>
+  );
 }
