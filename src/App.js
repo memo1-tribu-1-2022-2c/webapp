58b379663d9b5ec8f9fcb3cd4658ac9a4746dcd4
NCellini <ncellini@fi.uba.ar>
2022-11-27 15:00:00 -0300

    cambio navbar de bootstrap a chakra ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
+import {
+  Center,
+  Heading,
+  VStack,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from '@chakra-ui/react';
 import './App.css';
-import { Routes, Route} from 'react-router-dom';
 import Clientes from './Componentes_Soporte/Clientes';
 import Productos from './Componentes_Soporte/Productos';
 import Tickets from './Componentes_Soporte/Tickets';
 import NavbarGeneral from './Componentes_Soporte/NavbarGeneral';
+import {useState, useEffect} from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import {   
+  Routes,
+  Route,
+} from 'react-router-dom';
+
 
 function App() {
-  //const location = useLocation();
+  // 2. Wrap ChakraProvider at the root of your app
   return (
-    <>
+    <ChakraProvider>
       <NavbarGeneral />
-      <Routes>
-        <Route path="clientes" element={<Clientes />} />
-        <Route path="productos" element={<Productos />} />
-        <Route path="tickets" element={<Tickets />} />
-      </Routes>
-    </>
-  );
+        <Routes>
+          <Route path="clientes" element={<Clientes />} />
+          <Route path="productos" element={<Productos />} />
+          <Route path="tickets" element={<Tickets />} />
+        </Routes>
+      {/*
+      <Center padding={8}>
+        <VStack spacing={7}>
+          <Heading>Soporte</Heading>
+          <Tabs isFitted variant="soft-rounded">
+            <TabList mb="5em">
+              <Tab>Clientes</Tab>
+              <Tab>Productos</Tab>
+              <Tab>Tickets</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Clientes/>
+              </TabPanel>
+              <TabPanel>
+                <Productos/>
+              </TabPanel>
+              <TabPanel>
+                <Tickets/>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </VStack>
+      </Center>
+    */}
+    </ChakraProvider>
+  )
 }
-
 export default App;
--- src/Componentes_Soporte/Clientes.jsx
+++ src/Componentes_Soporte/Clientes.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 const Clientes = () => {
   return (
-    <h1>Estoy en Clientes</h1>
+    <Container>
+      <h1>Estoy en Clientes</h1>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg' width={100}/>
+    </Container>
   )
 }
 
