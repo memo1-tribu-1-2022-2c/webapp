4f75ad101af36ef146a6121faa3507a28a6f40fa
agustinghersi <agusghersi@gmail.com>
2022-11-27 19:15:08 -0300

    Update createProyect

--- src/pages/createProyect.js
+++ src/pages/createProyect.js
@@ -1,6 +1,5 @@
 import {
     Box,
-    Grid,
     Text,
     Input,
     Flex,
@@ -13,13 +12,35 @@ import {
         <>
             <Navbar/>
             <Box border='0px' mt='10' rounded='xl' bg='gray.300' mx='10' py='4'>
-                <Flex mx='3' justifyContent='space-between'>
+                <Flex mx='10' justifyContent='space-between'>
                     <Input border='0' width='xl' placeholder='Nombre del Proyecto'/>
                     <Flex gap={5}>
                         <Button borderRadius={'5'} fontSize={20}> Guardar Proyecto </Button>
                         <Button borderRadius={'5'} fontSize={20}> Descartar Proyecto </Button>
                     </Flex>
                 </Flex>
+                <Text mx='10' mt='16'>Descripción</Text>
+                <Box border='0px' mt='5' rounded='xl' bg='gray.100' mx='10' py='4'>
+                    <Input border='1'/>
+                </Box>
+                <Flex justifyContent='space-between'>
+                    <Text mx='10' mt='5'>Modulo</Text>
+                    <Box border='0px' mt='5' rounded='xl' bg='gray.100' mx='60' py='4'>
+                        <Input border='1' width='xl'/>
+                    </Box>
+                    <Text mx='10' mt='5'>Horas estimadas</Text>
+                    <Box border='0px' mt='5' rounded='xl' bg='gray.100' mx='60' py='4'>
+                        <Input border='1' width='xl'/>
+                    </Box>
+                </Flex>
+                <Text mx='10' mt='5'>Fecha de inicio</Text>
+                <Box border='0px' mt='5' rounded='xl' bg='gray.100' mx='10' py='4'>
+                    <Input border='1' width='xl'/>
+                </Box>
+                <Text mx='10' mt='5'>Fecha de finalización</Text>
+                <Box border='0px' mt='5' rounded='xl' bg='gray.100' mx='60' py='4'>
+                    <Input border='1' width='xl'/>
+                </Box>
             </Box>
         </>
     );
