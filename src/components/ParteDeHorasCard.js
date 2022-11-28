4ae7d1aa1a6dbd95c061512761dd0a93717411a1
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2022-11-28 19:43:40 -0300

    Copio vista del squad 2

--- src/components/ParteDeHorasCard.js
+++ src/components/ParteDeHorasCard.js
@@ -7,8 +7,10 @@ import {
 } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
 import {useNavigateWParams} from "../routes/navigation";
+import { Link, useSearchParams } from "react-router-dom";
 
 function ParteDeHorasCard(props) {
+  const [searchParams, _] = useSearchParams();
   const navigate = useNavigateWParams();
   const info = props.info;
   const nombre = "Parte " + info.tipo + " " + info.fechaInicio.toString();
@@ -18,21 +20,18 @@ function ParteDeHorasCard(props) {
   const editar = () => navigate(info.id.toString() + "/edit");
 
   return (
-    <Box p="2" rounded="md">
-      <Flex mx="3" justifyContent="space-between">
-        <Flex gap={1}>
-          <Text>{nombre}</Text>
-        </Flex>
-        <IconButton icon={<EditIcon />} onClick={editar} />
-        <Text borderRadius="md" bg="blue.100" px="2" py="1" size="sm">
-          {info.estado}
-        </Text>
-      </Flex>
-      <Flex m="3" justifyContent="space-between">
-        <Text mx="3">{horas}</Text>
-        <Button onClick={masInfo}>Más info.</Button>
-      </Flex>
-    </Box>
+    <Box p='2' rounded='md'>
+            <Link to={{pathname: info.id, search: "?" + searchParams}}>
+                <Flex mx='3' justifyContent='space-between'>
+                    <Flex gap={1}>
+                        <Text>{info.id} - </Text>
+                        <Text>{nombre}</Text>
+                    </Flex>
+                    <Text borderRadius='md' bg='blue.100' px='2' py='1' size='sm'>{info.estado}</Text>
+                </Flex>
+                <Text mt='5' mx='3'>{horas}</Text>
+            </Link>
+        </Box>
   );
 }
 
--- src/pages/recursos/ListadoDePartes.js
+++ src/pages/recursos/ListadoDePartes.js
@@ -1,4 +1,4 @@
-import { Button, Flex, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, GridItem, Input, Select } from "@chakra-ui/react";
 
 import ParteDeHorasCard from "../../components/ParteDeHorasCard";
 import {useNavigateWParams} from "../../routes/navigation";
@@ -11,25 +11,51 @@ function ListadoDePartes(props) {
   const partes = props.partes;
   return (
     <>
-      <Flex>
-        <Button onClick={crearParte}>CrearParte</Button>
-      </Flex>
-      <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-        {partes.map((value, index) => (
-          <GridItem
-            bg="white"
-            key={index}
-            w="70%"
-            h="150"
-            border="0px"
-            rounded={"md"}
-          >
-            <ParteDeHorasCard info={value} />
-          </GridItem>
-        ))}
-      </Grid>
+        <Flex bg='gray.300' mx='10' p='10' rounded='sm' mt='5' justifyContent='space-between'>
+            <Flex gap={10}>
+                <Input bg='white' width='xl' placeholder='Buscar proyecto...'/>
+                <Select bg='white' placeholder='Filtrar por...' width='60'>
+                    <option value="Nuevo">Nuevo</option>
+                    <option value="Finalizado">Finalizado</option>
+                    <option value="En progreso">En progreso</option>
+                    <option value="Pausado">Pausado</option>
+                    <option value="Cancelado">Cancelado</option>
+                </Select>
+            </Flex>
+            <Button borderRadius={'5'} fontSize={20} >Crear nuevo proyecto</Button>
+        </Flex>
+        <Box 
+            overflowY='auto' 
+            m='10' 
+            maxH='full' 
+            rounded='sm' 
+            bg='gray.300' 
+            py='10'
+            css={{
+                '&::-webkit-scrollbar': {
+                  width: '4px',
+                },
+                '&::-webkit-scrollbar-track': {
+                  width: '6px',
+                },
+                '&::-webkit-scrollbar-thumb': {
+                  background: 'gray',
+                  borderRadius: '24px',
+                }
+            }}
+        >
+            <Box pl='40' py='5'>
+                <Grid templateColumns='repeat(2, 1fr)' gap={6}>
+                    { partes.map((value,index) => (
+                        <GridItem bg='white' key={index} w='80%' h='150' rounded={'md'} >
+                            <ParteDeHorasCard info={value} path={`/proyectsList/${value.id}`}/>
+                        </GridItem>
+                    ))}
+                </Grid>
+            </Box>
+        </Box>
     </>
-  );
+);;
 }
 
 export default ListadoDePartes;
