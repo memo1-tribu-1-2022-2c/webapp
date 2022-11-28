895fcf161ac31f3510f205cac743defceba8e82f
agustinghersi <agusghersi@gmail.com>
2022-11-27 22:26:15 -0300

    Update createProyect

--- src/pages/createProyect.js
+++ src/pages/createProyect.js
@@ -6,17 +6,24 @@ import {
     Button
   } from '@chakra-ui/react'
   import Navbar from '../components/Navbar'
+  import { useNavigate } from "react-router-dom";
 
   function CreateProyect() {
+
+    const navigate = useNavigate()
+    const handleCreateProyect = () => {
+        navigate("/proyectsList")
+    }
+
     return (
         <>
             <Navbar/>
-            <Box border='0px' mt='10' rounded='xl' bg='gray.300' mx='10' py='4'>
+            <Box border='0px' mt='10' rounded='xl' bg='gray.300' mx='10' py='4' mb='5'>
                 <Flex mx='10' justifyContent='space-between'>
                     <Input border='0' width='xl' placeholder='Nombre del Proyecto'/>
                     <Flex gap={5}>
                         <Button borderRadius={'5'} fontSize={20}> Guardar Proyecto </Button>
-                        <Button borderRadius={'5'} fontSize={20}> Descartar Proyecto </Button>
+                        <Button borderRadius={'5'} fontSize={20} onClick={() => handleCreateProyect()}> Descartar Proyecto </Button>
                     </Flex>
                 </Flex>
                 <Text mx='10' mt='16'>Descripción</Text>
@@ -24,22 +31,26 @@ import {
                     <Input border='1'/>
                 </Box>
                 <Flex justifyContent='space-between'>
-                    <Text mx='10' mt='5'>Modulo</Text>
-                    <Box border='0px' mt='5' rounded='xl' bg='gray.100' mx='60' py='4'>
-                        <Input border='1' width='xl'/>
+                    <Box>
+                        <Text mx='10' mt='5'>Modulo</Text>
+                        <Box border='0px' mt='5' rounded='xl' bg='gray.100' mx='10' py='2'>
+                            <Input border='1' width='xl'/>
+                        </Box>
                     </Box>
-                    <Text mx='10' mt='5'>Horas estimadas</Text>
-                    <Box border='0px' mt='5' rounded='xl' bg='gray.100' mx='60' py='4'>
-                        <Input border='1' width='xl'/>
+                    <Box>
+                        <Text mx='10' mt='5'>Horas estimadas</Text>
+                        <Box border='0px' mt='5' rounded='xl' bg='gray.100' mx='10' py='2'>
+                            <Input border='1' width='xl'/>
+                        </Box>
                     </Box>
                 </Flex>
                 <Text mx='10' mt='5'>Fecha de inicio</Text>
-                <Box border='0px' mt='5' rounded='xl' bg='gray.100' mx='10' py='4'>
-                    <Input border='1' width='xl'/>
+                <Box border='0px' mt='5' rounded='xl' bg='gray.100' mx='10' py='2' width='xl'>
+                    <Input border='1'/>
                 </Box>
                 <Text mx='10' mt='5'>Fecha de finalización</Text>
-                <Box border='0px' mt='5' rounded='xl' bg='gray.100' mx='60' py='4'>
-                    <Input border='1' width='xl'/>
+                <Box border='0px' mt='5' rounded='xl' bg='gray.100' mx='10' py='2' width='xl'>
+                    <Input border='1'/>
                 </Box>
             </Box>
         </>
