bc51335185157a6a6858a6ab66be829f69d7c167
agustinghersi <agusghersi@gmail.com>
2022-11-29 13:01:34 -0300

    Update endpoint projects/get

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,16 +12,16 @@ function Card({info, path}) {
             <Link to={{pathname: path}}>
                 <Flex mx='3' justifyContent='space-between'>
                     <Flex gap={1}>
-                        <Text>{info.id} - </Text>
-                        <Text>{info.nombre}</Text>
+                        <Text>{info.projectId} - </Text>
+                        <Text>{info.name}</Text>
                     </Flex>
-                    <Text borderRadius='md' bg='blue.100' px='2' py='1' size='sm'>{info.estado}</Text>
+                    <Text borderRadius='md' bg='blue.100' px='2' py='1' size='sm' fontWeight='bold'>{info.state}</Text>
                 </Flex>
                 <Flex mt='5' justifyContent='space-between' mx='3'>
-                    <Text>Iniciado {info.fechaInicio}</Text>
-                    <Text fontWeight='bold'>{info.tipoDeProyecto}</Text>
+                    <Text>Iniciado {info.startingDate}</Text>
+                    <Text fontWeight='bold'>{info.projectType}</Text>
                 </Flex>
-                <Text mt='5' mx='3'>Finalización en {info.finalizacionEstimada}</Text>
+                <Text mt='5' mx='3'>Finalización en {info.endingDate}</Text>
             </Link>
         </Box>
     );
--- src/pages/proyectsList.js
+++ src/pages/proyectsList.js
@@ -10,6 +10,7 @@ import {
 import Navbar from '../components/Navbar'
 import ProyectCard from '../components/Card'
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react';
 
 const proyectos = [
 {
@@ -72,10 +73,38 @@ const proyectos = [
 
 function ProyectsList() {
 
+    const [projects, setProjects] = useState([])
+    const [projectsFilter, setProjectsFilter] = useState([])
+
     const navigate = useNavigate()
     const handleCreateProyect = () => {
         navigate("/proyectsList/createProyect")
     }
+
+    const wrapperLoadProyects = async() => {
+        await loadProyects()
+    }
+
+    const loadProyects = async() => {
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        const response = await fetch("https://squad2-2022-2c.herokuapp.com/api/v1/projects/all", requestOptions)
+        const responseData = await response.json()
+        setProjects(responseData)
+        setProjectsFilter(responseData)
+    }
+
+    const filterProjects = (value) => {
+        let filters = projects.filter(item => item.state === value)
+        setProjectsFilter(filters)
+    }
+
+    useEffect(() => {
+        wrapperLoadProyects()
+    }, [])
 
     return (
         <>
@@ -83,7 +112,7 @@ function ProyectsList() {
             <Flex bg='gray.300' mx='10' p='10' rounded='sm' mt='5' justifyContent='space-between'>
                 <Flex gap={10}>
                     <Input bg='white' width='xl' placeholder='Buscar proyecto...'/>
-                    <Select bg='white' placeholder='Filtrar por...' width='60'>
+                    <Select bg='white' placeholder='Filtrar por...' width='60' /* onChange={(value) => filterProjects(value)} */>
                         <option value="Nuevo">Nuevo</option>
                         <option value="Finalizado">Finalizado</option>
                         <option value="En progreso">En progreso</option>
@@ -115,9 +144,9 @@ function ProyectsList() {
             >
                 <Box pl='40' py='5'>
                     <Grid templateColumns='repeat(2, 1fr)' gap={6}>
-                        { proyectos.map((value,index) => (
-                            <GridItem bg='white' key={index} w='80%' h='150' rounded={'md'} >
-                                <ProyectCard info={value} path={`/proyectsList/${value.id}`}/>
+                        { projectsFilter.map((item) => (
+                            <GridItem bg='white' key={item.projectId} w='80%' h='150' rounded={'md'} >
+                                <ProyectCard info={item} path={`/proyectsList/${item.projectId}`}/>
                             </GridItem>
                         ))}
                     </Grid>
