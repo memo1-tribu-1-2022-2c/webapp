3286fb0e482e9c0f0235d69cc6c06a3fcd78c380
Gabriel-Re <gre@fi.uba.ar>
2022-11-29 11:34:05 -0300

    feat: Botones ticket

--- src/Componentes_Soporte/Tickets.jsx
+++ src/Componentes_Soporte/Tickets.jsx
@@ -1,4 +1,4 @@
-import { Center, ChakraProvider } from '@chakra-ui/react';
+import { Box, Button, Center, ChakraProvider, Flex, HStack } from '@chakra-ui/react';
 import React from 'react';
 import NavbarGeneral from './/NavbarGeneral';
 
@@ -6,9 +6,21 @@ const Tickets = () => {
   return (
     <ChakraProvider>
       <NavbarGeneral />
-      <Center>
-        <h1>Estoy en Tickets</h1>
-      </Center>
+      <Flex padding={5}>
+        <HStack>
+          <Box pr={20}></Box>
+          <Button pr={8}>Crear Ticket</Button>
+          {/* Villereada para poder hacer espacios jsajsa ayuda  */}
+          <Box pr={20}></Box>
+          <Box pr={20}></Box>
+          <Box pr={20}></Box>
+          <Box pr={20}></Box>
+          <Box pr={20}></Box>
+          <Box pr={20}></Box>
+          <Button pl={8}>Consulta de tickets</Button>
+        </HStack>
+      </Flex>
+      <Flex></Flex>
     </ChakraProvider>
   )
 }
