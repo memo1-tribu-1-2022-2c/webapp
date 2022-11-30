15face3ca7a7c1c50833cc151548381abe3b80ff
Francisco Pereira <franciscopere987@gmail.com>
2022-11-30 19:06:16 -0300

    Productos de clientes mostrados en una tabla

--- src/Componentes_Soporte/subcomponentes/Client.jsx
+++ src/Componentes_Soporte/subcomponentes/Client.jsx
@@ -1,9 +1,31 @@
-import { Accordion, AccordionItem, AccordionButton, AccordionPanel, Box, Button, Tag, AccordionIcon } from '@chakra-ui/react'
+import { Accordion, AccordionItem, AccordionButton, AccordionPanel, Box, Button, Tag, AccordionIcon, TableContainer, Table, Thead, Tr, Th, Tbody, Td, Text } from '@chakra-ui/react'
+import axios from 'axios';
 import React from 'react'
+import ProductsTable from './ProductTable'
 
 const Client = ({ CUIT = 'cuit de prueba', razon_social = 'FIUBA' }) => {
 
-    console.log(CUIT + razon_social)
+    const [products, setProducts] = React.useState([]);
+
+    const [loading, setLoading] = React.useState(true);
+
+    const [error, setError] = React.useState(false);
+
+    const client_products = async () => {
+        const params = CUIT;
+
+        try{
+            const data = await (await axios.get("https://modulo-soporte.onrender.com/client/products", {params: {query: params}})).data
+            setProducts(data.products);
+        }catch{
+            setError(true);
+        }
+        setLoading(false);
+    }
+
+    React.useState(() => {
+        client_products()
+    })
 
     return (
         <Accordion  allowToggle>
@@ -17,8 +39,11 @@ const Client = ({ CUIT = 'cuit de prueba', razon_social = 'FIUBA' }) => {
                                 <AccordionIcon />
                             </AccordionButton>
                         </h2>
-                        <AccordionPanel pb={4} bg='white' borderTopColor='white' >
-                            Soy el cliente {CUIT}
+                        <AccordionPanel pb={4} bg='white' borderTopColor='white' justifyContent="center">
+                            {loading && <Text justifySelf="center">Cargando productos del cliente</Text>}
+                            {products.length !== 0 && <ProductsTable products={products}/>}
+                            {!loading && products.length == 0 && !error ? <Text>El cliente no tiene productos</Text> : null}
+                            {error ? <Text>Ocurrio un error al cargar los productos del cliente</Text> : null}
                         </AccordionPanel>
                     </>
                 )}
