22e1e147c0f6b2f25434340adab797323ff21355
Mirko <mirkus0824@gmail.com>
2022-12-01 18:35:11 -0300

    update roots

--- src/pages/proyectMain.js
+++ src/pages/proyectMain.js
@@ -1,11 +1,14 @@
-import { Routes, Route } from "react-router-dom";
-import CreateProyect from "./proyectos/createProyect";
-import EditProyect from "./proyectos/editProyect";
-import Proyect from "./proyectos/proyect";
-import ProyectsList from "./proyectos/proyectsList";
-import CreateTask from "./proyectos/createTask";
-import Task from "./proyectos/task";
-import EditTask from "./proyectos/editTask";
+import {
+  Routes,
+  Route
+} from 'react-router-dom'
+import ProyectsList from './proyectsList'
+import CreateProyect from './createProyect'
+import Proyect from './proyect'
+import CreateTask from './createTask'
+import EditProyect from './editProyect'
+import Task from './task'
+import EditTask from './editTask'
 import { NavigateWP } from "../routes/navigation";
 import React from "react";
 
@@ -16,23 +19,15 @@ function Proyectos(props) {
   }, [props]);
   return (
     <Routes>
+      
       <Route index element={<NavigateWP to="proyectsList" />} />
-      <Route path="proyectsList" element={<ProyectsList />} />
-      <Route path="proyectsList/createProyect" element={<CreateProyect />} />
-      <Route path="proyectsList/:proyectId" element={<Proyect />} />
-      <Route
-        path="proyectsList/:proyectId/editProyect"
-        element={<EditProyect />}
-      />
-      <Route
-        path="proyectsList/:proyectId/createTask"
-        element={<CreateTask />}
-      />
-      <Route path="proyectsList/:proyectId/:taskId" element={<Task />} />
-      <Route
-        path="proyectsList/:proyectId/:taskId/editTask"
-        element={<EditTask />}
-      />
+      {/* <Route path='/proyectsList' element={<ProyectsList/>}/> */}
+      <Route path='/proyectsList/createProyect' element={<CreateProyect/>}/>
+      <Route path='/proyectsList/:proyectId' element={<Proyect/>}/>
+      <Route path='/proyectsList/:proyectId/editProyect' element={<EditProyect/>}/>
+      <Route path='/proyectsList/:proyectId/createTask' element={<CreateTask/>}/>
+      <Route path='/proyectsList/:proyectId/:taskId' element={<Task/>}/>
+      <Route path='/proyectsList/:proyectId/:taskId/editTask' element={<EditTask/>}/>
     </Routes>
   );
 }
