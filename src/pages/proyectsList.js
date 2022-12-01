4a6221562b44a26fc2bcf591f79233dfbf03a610
agustinghersi <agusghersi@gmail.com>
2022-12-01 18:10:29 -0300

    update proyectList

--- src/pages/proyectsList.js
+++ src/pages/proyectsList.js
@@ -1,33 +1,33 @@
 import {
-  Box,
-  Grid,
-  Select,
-  Input,
-  GridItem,
-  Button,
-  Flex,
-} from "@chakra-ui/react";
-import Navbar from "../components/Navbar";
-import ProyectCard from "../components/Card";
+    Box,
+    Grid,
+    Select,
+    Input,
+    GridItem,
+    Button,
+    Flex
+} from '@chakra-ui/react'
+import Navbar from '../components/Navbar'
+import ProyectCard from '../components/Card'
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const proyectStates = ["TODOS", "NUEVO", "FINALIZADO", "EN_PROGRESO", "PAUSADO", "CANCELADO"]
 
 function ProyectsList() {
+
     const [projects, setProjects] = useState([])
     const [projectsFilter, setProjectsFilter] = useState([])
     const [proyectState, setProyectState] = useState("")
 
-  const wrapperLoadProyects = async () => {
-    await loadProyects();
-  };
+    const navigate = useNavigate()
+    const handleCreateProyect = () => {
+        navigate("/proyectsList/createProyect")
+    }
 
-  const loadProyects = async () => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
+    const wrapperLoadProyects = async() => {
+        await loadProyects()
+    }
 
     const loadProyects = async() => {
         const requestOptions = {
@@ -112,94 +112,6 @@ function ProyectsList() {
             </Box>
         </>
     );
-    const responseData = await response.json();
-    setProjects(responseData);
-    setProjectsFilter(responseData);
-  };
-
-  const filterProjects = (value) => {
-    let filters = projects.filter((item) => item.state === value);
-    setProjectsFilter(filters);
-  };
-
-  useEffect(() => {
-    wrapperLoadProyects();
-  }, []);
-
-  return (
-    <>
-      <Navbar isProyectsPage={true} />
-      <Flex
-        bg="gray.300"
-        mx="10"
-        p="10"
-        rounded="sm"
-        mt="5"
-        justifyContent="space-between"
-      >
-        <Flex gap={10}>
-          <Input bg="white" width="xl" placeholder="Buscar proyecto..." />
-          <Select
-            bg="white"
-            placeholder="Filtrar por..."
-            width="60" /* onChange={(value) => filterProjects(value)} */
-          >
-            <option value="Nuevo">Nuevo</option>
-            <option value="Finalizado">Finalizado</option>
-            <option value="En progreso">En progreso</option>
-            <option value="Pausado">Pausado</option>
-            <option value="Cancelado">Cancelado</option>
-          </Select>
-        </Flex>
-        <Button
-          borderRadius={"5"}
-          fontSize={20}
-          onClick={() => handleCreateProyect()}
-        >
-          Crear nuevo proyecto
-        </Button>
-      </Flex>
-      <Box
-        overflowY="auto"
-        m="10"
-        maxH="full"
-        rounded="sm"
-        bg="gray.300"
-        py="10"
-        css={{
-          "&::-webkit-scrollbar": {
-            width: "4px",
-          },
-          "&::-webkit-scrollbar-track": {
-            width: "6px",
-          },
-          "&::-webkit-scrollbar-thumb": {
-            background: "gray",
-            borderRadius: "24px",
-          },
-        }}
-      >
-        <Box pl="40" py="5">
-          <Grid templateColumns="repeat(2, 1fr)" gap={6}>
-            {projectsFilter.map((item) => (
-              <GridItem
-                bg="white"
-                key={item.projectId}
-                w="80%"
-                h="150"
-                rounded={"md"}
-              >
-                <ProyectCard
-                  info={item}
-                  path={`/proyectsList/${item.projectId}`}
-                />
-              </GridItem>
-            ))}
-          </Grid>
-        </Box>
-      </Box>
-    </>
-  );
 }
-
+  
 export default ProyectsList;
