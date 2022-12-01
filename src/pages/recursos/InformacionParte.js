f565ee25a3bdb08a01b690b5d0ccb02308c4013a
matias <mfusco@fi.uba.ar>
2022-12-01 12:34:26 -0300

    Agregamos menu de registros de horas

--- src/pages/recursos/InformacionParte.js
+++ src/pages/recursos/InformacionParte.js
@@ -5,23 +5,25 @@ import {
   TableCaption,
   Thead,
   Tr,
+  Td,
   Th,
   Tbody,
   IconButton,
-  Popover,
-  PopoverTrigger,
-  Portal,
-  PopoverContent,
-  PopoverArrow,
-  PopoverHeader,
-  PopoverCloseButton,
-  PopoverBody,
-  Stack,
-  NumberInput,
-  NumberInputStepper,
-  NumberIncrementStepper,
-  NumberDecrementStepper,
-  NumberInputField,
+  Button,
+  Text,
+} from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  FormLabel,
+  Select,
+  Input,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
@@ -48,60 +50,14 @@ const horasParte = [
   },
 ];
 
-function RegistroHoras(props) {
-  const [nuevasHoras, setNuevasHoras] = useState(props.registro.horas);
-  const handleCambio = (value) => setNuevasHoras(value);
-  return (
-    <Tr>
-      <Th>{props.registro.fecha}</Th>
-      <Th>{`${props.registro.tipo}: ${props.registro.id}`}</Th>
-      <Th isNumeric>{props.registro.horas}</Th>
-      <Th>
-        <Popover isLazy>
-          <PopoverTrigger>
-            <IconButton icon={<EditIcon />} />
-          </PopoverTrigger>
-          <Portal>
-            <PopoverContent>
-              <PopoverArrow />
-              <PopoverHeader>Modificar horas</PopoverHeader>
-              <PopoverCloseButton />
-              <PopoverBody>
-                <Stack w="full" direction="row">
-                  <NumberInput
-                    defaultValue={props.registro.horas}
-                    onChange={handleCambio}
-                  >
-                    <NumberInputField />
-                    <NumberInputStepper>
-                      <NumberIncrementStepper />
-                      <NumberDecrementStepper />
-                    </NumberInputStepper>
-                  </NumberInput>
-                  <IconButton
-                    icon={<CheckIcon />}
-                    onClick={() => props.onEditar(nuevasHoras)}
-                  >
-                    Button
-                  </IconButton>
-                </Stack>
-              </PopoverBody>
-            </PopoverContent>
-          </Portal>
-        </Popover>
-      </Th>
-      <Th>
-        <IconButton icon={<DeleteIcon />} onClick={props.onEliminar} />
-      </Th>
-    </Tr>
-  );
-}
-
 function InformacionParte() {
   const contexto = GetContextoRecursos();
   const { id } = useParams();
   const parte = contexto.partes.getPartes().find((p) => p.id === id);
   const nombre = "Parte " + parte.tipo + " " + parte.fechaInicio.toString();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [creandoNuevoRegistro, setCreandoNuevoRegistro] = useState(false);
 
   const crearHandleEditar = (key) => {
     return (nuevasHoras) => {
@@ -116,35 +72,96 @@ function InformacionParte() {
   };
 
   return (
-    <TableContainer>
-      <Table variant="simple">
-        <TableCaption placement="top" fontSize={32} mb={6}>
-          Horas registradas en el parte {nombre}
-        </TableCaption>
-        <Thead>
-          <Tr>
-            <Th>Fecha</Th>
-            <Th>Descripción</Th>
-            <Th isNumeric>Horas</Th>
-            <Th>Editar</Th>
-            <Th>Eliminar</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {horasParte.map((registro, _) => {
-            const key = registro.tipo + registro.id;
-            return (
-              <RegistroHoras
-                key={key}
-                onEditar={crearHandleEditar(key)}
-                onEliminar={crearHandleEliminar(key)}
-                registro={registro}
-              />
-            );
-          })}
-        </Tbody>
-      </Table>
-    </TableContainer>
+    <>
+      <Button
+        marginTop={5}
+        marginLeft={5}
+        onClick={() => {
+          setCreandoNuevoRegistro(true);
+          onOpen();
+        }}
+      >
+        {" "}
+        Crear registro{" "}
+      </Button>
+      <TableContainer>
+        <Table variant="simple">
+          <TableCaption placement="top" fontSize={32} mb={6}>
+            Horas registradas en el parte {nombre}
+          </TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Fecha</Th>
+              <Th>Descripción</Th>
+              <Th isNumeric>Horas</Th>
+              <Th>Editar</Th>
+              <Th>Eliminar</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {horasParte.map((registro, index) => {
+              return (
+                <Tr key={index}>
+                  <Td>{registro.fecha}</Td>
+                  <Td>{registro.tipo}</Td>
+                  <Td isNumeric>{registro.horas}</Td>
+                  <Th>
+                    <IconButton
+                      onClick={() => {
+                        setCreandoNuevoRegistro(false);
+                        onOpen();
+                      }}
+                      icon={<EditIcon />}
+                    />
+                  </Th>
+                  <Th>
+                    <IconButton icon={<DeleteIcon />} />
+                  </Th>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      </TableContainer>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{creandoNuevoRegistro ? ("Crear Registro") : ("Editar Registro")}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormLabel>Concepto/Tarea</FormLabel>
+            <Select w={"30%"} bg="white" placeholder="Tipo..." width="60">
+              <option value="CONCEPTO">Concepto</option>
+              <option value="TAREA">Tarea</option>
+            </Select>
+            <Input marginTop={5} bg="white" placeholder={"ID..."} />
+            <FormLabel marginTop={5}>Cantidad de Horas</FormLabel>
+            <Select w={"30%"} bg="white" placeholder="Cantidad..." width="60">
+              {[...Array(24).keys()]
+                .map((p) => {
+                  return p + 1;
+                })
+                .map((p) => {
+                  return (
+                    <option key={p} value={p}>
+                      {p}
+                    </option>
+                  );
+                })}
+            </Select>
+            <FormLabel marginTop={5}>Dia</FormLabel>
+            <Input type="date" />
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Aplicar</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
 
